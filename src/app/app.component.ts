12a69eefdc5012c1e600589b2c15831ee60b8609
Joseph Williams <josephwilliams@Josephs-MacBook-Pro.local>
2017-09-27 14:40:24 -0400

    change css ext to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
