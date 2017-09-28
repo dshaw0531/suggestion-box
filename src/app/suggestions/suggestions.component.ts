d542121bf148a37441358ae2742b342898a0f86d
Deanna Shaw <dshaw@reliaslearning.com>
2017-09-28 09:39:29 -0400

    Confirm delete modal

--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SuggestionService } from '../shared/suggestion.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-suggestions',
@@ -8,19 +9,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./suggestions.component.scss']
 })
 export class SuggestionsComponent implements OnInit {
-
+  @ViewChild('confirmDeleteModal') confirmDeleteModal: ElementRef;
   public suggestions: any;
   public success = false;
+  openModalRef: NgbModalRef;
+  currentSuggestion: any;
 
-  constructor(private suggestionService: SuggestionService, private route: ActivatedRoute) {
-    this.route = route;
+  constructor(private suggestionService: SuggestionService, public modalService: NgbModal, private route: ActivatedRoute) {
     this.suggestionService = suggestionService;
     this.suggestions = {};
+    this.route = route;
    }
 
   ngOnInit() {
     this.suggestions = this.suggestionService.getSuggestions();
     this.success = this.route.snapshot.params['success'];
+  }
+
+  openDeleteModal(deleteItem: any): void {
+    this.currentSuggestion = deleteItem;
+    this.openModalRef = this.modalService.open(this.confirmDeleteModal, { windowClass: 'modal-wrapper-sm' });
   }
 
   deleteSuggestion(suggestionKey: any) {
