7a34e02760a3d02c013b94d1ad1ef31d48009260
Deanna Shaw <dshaw@reliaslearning.com>
2017-09-28 15:56:23 -0400

    Beginning of auth service and login modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('loginModal') loginModal: ElementRef;
+  openModalRef: NgbModalRef;
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(public authService: AuthService, public modalService: NgbModal) {}
+
+  openLoginModal(): void {
+    this.openModalRef = this.modalService.open(this.loginModal, { windowClass: 'modal-wrapper-sm' });
+  }
+
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
   ngOnInit() {
   }
